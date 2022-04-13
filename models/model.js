b6719a91664882c82e53f3f6f6d01616d1850d69
ninomnn <ninomnn.code@gmail.com>
2022-04-13 20:04:01 +0200

    first commit

---
+++ models/model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    _id: {
+        required: true,
+        type: Number
+    },
+    name: {
+        required: true,
+        type: String
+    },
+    type: {
+        required: true,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    rating: {
+        required: true,
+        type: Number
+    },
+    warranty_years: {
+        required: true,
+        type: Number
+    },
+    available: {
+        required: true,
+        type: Boolean
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
